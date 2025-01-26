1567567f4cae9e5c484c6558bd0ee0e55d41ce9d
강희영 <monster937@gentlemonster.com>
2025-01-26 15:20:50 +0900

    설명 주석

--- src/main/java/com/example/hello_spring/metro/client/SeoulMetroInterfaceClient.java
+++ src/main/java/com/example/hello_spring/metro/client/SeoulMetroInterfaceClient.java
@@ -6,6 +6,7 @@ import org.springframework.web.service.annotation.HttpExchange;
 
 @HttpExchange// HTTP 클라이언트 인터페이스
 public interface SeoulMetroInterfaceClient {
+    //외부로 연동되는 API 목록 (사용하는 리스트와 request, response 값을 명확하게 관리할 수 있음)
 
     @GetExchange("/xml/SearchSTNBySubwayLineInfo/{startPage}/{endPage}")
     String getSeoulMetroStation(
